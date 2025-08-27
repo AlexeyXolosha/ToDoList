d06ff2ae156a8f0c113a69370f0ef7e1c751b2d0
AlexeyXolosha <alexeyxolosh4@gmail.com>
2025-08-27 11:30:42 +0500

    js start

---
+++ src/js/app.js
@@ -0,0 +1,45 @@
+class Todo {
+    selectors = {
+        root: '[data-js-todo]',
+        newTaskForm: '[data-js-todo-new-task-form]',
+        newTaskInput: '[data-js-todo-new-task-input]',
+        searchTaskFrom: '[data-js-todo-search]',
+        searchTaskInput: '[data-js-todo-search-input]',
+        totalTasks: '[data-js-todo-total-tasks]',
+        deleteAllButton: '[data-js-todo-delete-all-button]',
+        list: '[data-js-todo-list]',
+        item: '[data-js-todo-item]',
+        itemCheckbox: '[data-js-todo-item-checkbox]',
+        itemLabel: '[data-js-todo]',
+        itemButtonDelete: '[data-js-todo-item-delete-button]',
+        emptyMessage: '[data-js-todo-empty-message]',
+    }
+
+    stateClasses = {
+        isVisible: 'is-visible',
+        isDisappearing: 'is-disappearing'
+    }
+
+    localStorageKey = 'todo-items'
+
+    constructor() {
+        this.rootElement = document.querySelector(this.selectors.root)
+        this.newTaskFormElement = this.rootElement.querySelector(this.selectors.newTaskForm)
+        this.newTaskInputElement = this.rootElement.querySelector(this.selectors.newTaskInput)
+        this.searchTaskFormElement = this.rootElement.querySelector(this.selectors.searchTaskForm)
+        this.searchTaskInputElement = this.rootElement.querySelector(this.selectors.searchTaskInput)
+        this.totalTasksElement = this.rootElement.querySelector(this.selectors.totalTasks)
+        this.deleteAllButtonElement = this.rootElement.querySelector(this.selectors.deleteAllButton)
+        this.listElement = this.rootElement.querySelector(this.selectors.list)
+        this.emptyMessageElement = this.rootElement.querySelector(this.selectors.emptyMessage)
+        this.state = {
+            items: this.getItemsFromLocalStorage(),
+            filteredItems: null,
+            searchQuery: '',
+        }
+        this.render()
+        this.bindEvents()
+    }
+}
+
+new Todo()
